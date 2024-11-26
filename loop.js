da1fb0df5682e60a4a40297c7a506c4e515df877
n6s8 <157233187+n6s8@users.noreply.github.com>
2024-11-26 16:42:59 +0500

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,10 @@
+let total = 0;
+for (let i = 2; i < 101; i+=2) {
+    total += i;
+}
+console.log(`total sum equals to ${total}`)
+let j = 0;
+while (j < 3) {
+    alert(`number ${j}`)
+    j++;
+}
